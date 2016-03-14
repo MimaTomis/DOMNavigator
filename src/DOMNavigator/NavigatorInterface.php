93401bafb2315f53ee5498fc671f3a732f31950f
Mafiosi <dnkonev@yandex.ru>
2016-03-14 22:25:13 +0300

    fix navigator interface

--- src/DOMNavigator/NavigatorInterface.php
+++ src/DOMNavigator/NavigatorInterface.php
@@ -3,5 +3,33 @@ namespace DOMNavigator;
 
 interface NavigatorInterface
 {
+	/**
+	 * Load html document by url, file path or string content
+	 *
+	 * @param string $content
+	 * @param string $encoding
+	 *
+	 * @return $this
+	 */
+	public function loadHTML($content, $encoding = 'utf-8');
 
+	/**
+	 * Load xml document by url, file path or string content
+	 *
+	 * @param string $content
+	 * @param string $encoding
+	 *
+	 * @return $this
+	 */
+	public function loadXML($content, $encoding = 'utf-8');
+
+	/**
+	 * Navigate in loaded document
+	 *
+	 * @param $query
+	 * @param \DOMElement|null $context
+	 *
+	 * @return \DOMNodeList
+	 */
+	public function navigate($query, \DOMElement $context = null);
 }
